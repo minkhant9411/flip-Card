3b50fd67db1e8ee91b78d2825e7cd41497dcd1b4
minkhant <minkhant9411@gmail.com>
2021-06-06 11:24:07 +0630

    only card flip

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
 const cardTag = document.getElementsByClassName('card');
 const resultTag = document.querySelector('.result');
-let click = 1;
-
+let flip = false;
+let click = 0;
+let imgHolder1;
+let imgHolder2;
+let temp;
 let score = 0;
 for (let i = 0; i < cardTag.length; i++) {
   const innerCardTag = document.getElementsByClassName('inner-card')[i];
-  const imageTag = document.getElementsByTagName('img')[i].className;
-  const imageTag_p = document.getElementsByTagName('img')[i];
-
-  innerCardTag.addEventListener('click', () => {
-    const rotatefun = () => {
-      if (innerCardTag.classList.contains('rotate')) {
-        innerCardTag.classList.remove('rotate');
-      } else {
-        innerCardTag.classList.add('rotate');
-      }
-    };
-    const classRmWithTime = () => {
-      setTimeout(() => {
-        imgHolder1.classList.remove('rotate');
-        imgHolder2.classList.remove('rotate');
-      }, 1000);
-    };
-    rotatefun();
-    click++;
-  });
+  const imageClassName = document.getElementsByTagName('img')[i].className;
+  const parentOfimageTag =
+    document.getElementsByTagName('img')[i].parentElement.parentElement;
+  innerCardTag.addEventListener('click', () => {});
 }
