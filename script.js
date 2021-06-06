c34472d2159b11d1c9b3b5a3517fddc410431222
minkhant <minkhant9411@gmail.com>
2021-06-06 11:42:58 +0630

    add function flip when click

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const cardTag = document.getElementsByClassName('card');
 const resultTag = document.querySelector('.result');
 let flip = false;
-let click = 0;
-let imgHolder1;
-let imgHolder2;
-let temp;
-let score = 0;
+
 for (let i = 0; i < cardTag.length; i++) {
   const innerCardTag = document.getElementsByClassName('inner-card')[i];
   const imageClassName = document.getElementsByTagName('img')[i].className;
-  const parentOfimageTag =
-    document.getElementsByTagName('img')[i].parentElement.parentElement;
-  innerCardTag.addEventListener('click', () => {});
+
+  innerCardTag.addEventListener('click', function () {
+    if (this.classList.contains('rotate')) {
+      this.classList.remove('rotate');
+    } else {
+      this.classList.add('rotate');
+    }
+  });
 }
